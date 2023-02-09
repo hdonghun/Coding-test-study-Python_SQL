ec56ab50c0a037c6fa131cfe584befc4b22d160f
Donghun HAN/韩东勳 <67058000+hdonghun@users.noreply.github.com>
2023-02-09 22:30:40 +0900

    출력시 소수점만 없에고 싶을떄, int(x)
    출력시 소수점만 없에고 싶을떄, int(x)

---
+++ Python/69. Sqrt(x).py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/sqrtx/
+
+# x = 17
+# y = 0
+
+# print(x**(1/2))
+# print(x**(1/3))
+# print(x**(1/4))
+# print(x**(1/5))
+# print(x**(1/6))
+# print(int(x**(1/2)))
+
+# 나의방법
+class Solution:
+    def mySqrt(self, x: int) -> int:
+
+        return (int(x**(1/2)))
+
+
+
+ 
+
+
